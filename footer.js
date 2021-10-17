aeaa582fa3b183f66d6f96ff7c5b1e84561ec9a1
hunkier <huangkuier@gmail.com>
2021-10-17 08:17:07 +0800

    update

---
+++ footer.js
@@ -0,0 +1,9 @@
+
+var href = window.location.href;
+var icp_container = document.getElementById('icp_container');
+var content='<a href="http://beian.miit.gov.cn" target="_blank">赣ICP备20001211号-1</a>';
+var site='hunkier.cn';
+// site = 'localhost';
+if (href.indexOf(site) >-1){
+  icp_container.innerHTML=content;
+}
